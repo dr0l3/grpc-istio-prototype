d170523213f02ad99eedb610d36830358d10460b
Rune Pedersen <rdp@dixa.com>
2020-01-23 12:56:14 +0100

    Header routing

--- rest/src/main/scala/RestApi.scala
+++ rest/src/main/scala/RestApi.scala
@@ -5,19 +5,41 @@ import org.http4s.dsl.io._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import cats.implicits._
-import io.grpc.ManagedChannelBuilder
+import io.grpc.{CallOptions, Channel, ClientCall, ClientInterceptor, ClientInterceptors, Context, ForwardingClientCall, ManagedChannelBuilder, Metadata, MethodDescriptor}
 import org.http4s.server.blaze._
 import org.http4s.implicits._
 import org.http4s.server.Router
 import users.users.{CreateUserRequest, GetUserRequest, User, UserServiceGrpc}
 import org.http4s.circe._
 import io.circe.generic.auto._
+import io.grpc.Metadata.Key
+import io.grpc.stub.MetadataUtils
 import org.http4s.circe.CirceEntityEncoder._
-
 import pureconfig._
 import pureconfig.generic.auto._
 
+import scala.concurrent.Future
+
 case class RestApiConfig(userAddress: Option[String], conversationAddress: Option[String])
+
+
+class EnviromentInterceptor extends ClientInterceptor {
+  override def interceptCall[ReqT, RespT](method: MethodDescriptor[ReqT, RespT], callOptions: CallOptions, next: Channel): ClientCall[ReqT, RespT] = {
+    new ForwardingClientCall.SimpleForwardingClientCall[ReqT, RespT](next.newCall(method, callOptions)) {
+      override def start(responseListener: ClientCall.Listener[RespT], headers: Metadata): Unit =  {
+        println("CLIENT INTERCEPTOR")
+        println(headers)
+        println(Constants.envContext.get())
+        headers.put(Metadata.Key.of("x-custom-header", Metadata.ASCII_STRING_MARSHALLER), Constants.envContext.get())
+        super.start(responseListener, headers)
+      }
+    }
+  }
+}
+
+object Constants {
+  val envContext = Context.keyWithDefault("env", "base")
+}
 
 object RestApi extends IOApp{
 
@@ -26,31 +48,41 @@ object RestApi extends IOApp{
     throw new RuntimeException("FAIL")
   }, config => config)
 
-  val channel = ManagedChannelBuilder.forAddress(config.userAddress.getOrElse("localhost"), 8081).usePlaintext().build()
+  val baseChan = ManagedChannelBuilder.forAddress(config.userAddress.getOrElse("localhost"), 8081).usePlaintext().build()
+  val channel = ClientInterceptors.intercept(baseChan,new EnviromentInterceptor())
 
-  val userClient = UserServiceGrpc.stub(channel)
+  val userClient = UserServiceGrpc.stub(baseChan).build(channel, CallOptions.DEFAULT)
 
 
-  def getUser(userId: Int): IO[Option[User]] = {
-    println(s"Attempt to find user with id $userId")
-    val res = IO(userClient.getById(GetUserRequest(userId)))
-    IO.fromFuture(res).map{res =>
-      println(res.user)
-      res
-    }
-      .map(_.user)
+  def getUser(userId: Int, environment: String): Future[Option[User]] = {
+    println(s"Attempt to find user with id $userId in $environment")
+    Context.current().withValue(Constants.envContext, environment).call(() =>{
+      userClient.getById(GetUserRequest(userId))
+        .map{res =>
+          println(res.user)
+          res
+        }
+        .map(_.user)
+    })
   }
 
-  def createUser(name: String): IO[User]= {
-    IO.fromFuture(IO(userClient.create(CreateUserRequest(name))))
+  def createUser(name: String, environment: String): Future[User]= {
+    Context.current().withValue(Constants.envContext, environment).call(() => {
+      userClient.create(CreateUserRequest(name))
+    })
   }
 
+  def envFromReq(req: Request[IO]): String = {
+    val env = req.headers.find(_.name.value == "env").map(_.value).getOrElse("base")
+    println(s"Env: ${env}. Req ${req.headers}")
+    env
+  }
 
   val service = HttpRoutes.of[IO] {
-    case GET -> Root / "users" / IntVar(userId) =>
-      getUser(userId).flatMap(Ok(_))
-    case POST -> Root / "users" / name  =>
-      createUser(name).flatMap(Ok(_))
+    case req @ GET -> Root / "users" / IntVar(userId) =>
+      IO.fromFuture(IO(getUser(userId, envFromReq(req)))).flatMap(Ok(_))
+    case req @POST -> Root / "users" / name  =>
+      IO.fromFuture(IO(createUser(name, envFromReq(req)))).flatMap(Ok(_))
   }
 
   val services = service
--- users/src/main/scala/UserServiceImpl.scala
+++ users/src/main/scala/UserServiceImpl.scala
@@ -1,16 +1,15 @@
-import java.util.UUID
-
-import io.grpc.ServerBuilder
-import users.users.{CreateUserRequest, GetUserRequest, GetUserResponse, User, UserServiceGrpc}
+import io.grpc._
+import users.users._
 
 import scala.collection.mutable.ListBuffer
-import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, Future}
 
 class UserServiceImpl extends UserServiceGrpc.UserService{
   val userDb = ListBuffer.empty[User]
 
   override def create(request: CreateUserRequest): Future[User] = Future{
+    println("")
     val id = userDb.size
     val user = User(id, request.name)
     userDb.+=(user)
@@ -24,7 +23,7 @@ class UserServiceImpl extends UserServiceGrpc.UserService{
 }
 
 object UserRunner extends App {
-  val serverDef = UserServiceGrpc.bindService(new UserServiceImpl(), ExecutionContext.global)
+  val serverDef = ServerInterceptors.intercept(UserServiceGrpc.bindService(new UserServiceImpl(), ExecutionContext.global), new Zomg())
 
   val server = ServerBuilder
     .forPort(8081)
@@ -38,3 +37,59 @@ object UserRunner extends App {
 
     server.awaitTermination()
 }
+
+class Zomg extends ServerInterceptor {
+  override def interceptCall[ReqT, RespT](call: ServerCall[ReqT, RespT], headers: Metadata, next: ServerCallHandler[ReqT, RespT]): ServerCall.Listener[ReqT] = {
+    val current = Context.current()
+    println("SERVER INTERCEPT")
+    println(headers)
+    Contexts.interceptCall(current, call, headers, next)
+  }
+}
+
+class ZomgClinet extends ClientInterceptor {
+  override def interceptCall[ReqT, RespT](method: MethodDescriptor[ReqT, RespT], callOptions: CallOptions, next: Channel): ClientCall[ReqT, RespT] = {
+    new ForwardingClientCall.SimpleForwardingClientCall[ReqT, RespT](next.newCall(method, callOptions)) {
+      override def start(responseListener: ClientCall.Listener[RespT], headers: Metadata): Unit =  {
+        println("CLIENT INTERCEPTOR")
+        println(headers)
+        headers.put(Metadata.Key.of("x-custom-header", Metadata.ASCII_STRING_MARSHALLER), Constants.envContext.get())
+        super.start(responseListener, headers)
+      }
+    }
+  }
+}
+
+object Constants {
+  val envContext = Context.keyWithDefault("env", "base")
+}
+
+//object Headers extends App {
+//  val port = 9876
+//  val serverDef = ServerInterceptors.intercept(UserServiceGrpc.bindService(new UserServiceImpl(), ExecutionContext.global), new Zomg())
+//
+//  val server = ServerBuilder
+//    .forPort(port)
+//    .addService(serverDef)
+//    .build
+//    .start
+//
+//  val channel = ManagedChannelBuilder.forAddress("localhost", port).usePlaintext().build()
+//  val wht = ClientInterceptors.intercept(channel, new ZomgClinet())
+//
+//  val userClient = UserServiceGrpc.stub(channel).build(wht, CallOptions.DEFAULT)
+//
+//  Context.current().withValue(Constants.envContext, "rune").call(() => {
+//    val res = userClient.create(CreateUserRequest("rune"))
+//    println(res)
+//    res
+//  })
+//
+//  Context.current().withValue(Constants.envContext, "jens").run(() => {
+//    val res2 = userClient.getById(GetUserRequest(0))
+//    println(res2)
+//  })
+//
+//
+//  server.awaitTermination()
+//}
